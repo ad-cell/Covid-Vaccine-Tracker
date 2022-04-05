53e25e50742f55450f48cc12ca098908e43a7a41
Tiwari Aditya Sushil Kumar <tiwaria.sushilkumar.met19@itbhu.ac.in>
2022-04-05 23:33:08 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,23 @@
+const express= require("express");
+const bodyParser= require("body-parser");
+
+const app =express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+
+
+app.get("/",function(req,res){
+
+   res.sendFile(__dirname+"/public/html/index.html");
+})
+
+
+
+
+
+const PORT =process.env.PORT||3000;
+app.listen(PORT,function(){
+
+  console.log("server is running on port 3000");
+})
